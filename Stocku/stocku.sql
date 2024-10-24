58b999d6ed28b9c6ddebb64f7dff8386091bf908
madlanh <116224003+madlanh@users.noreply.github.com>
2024-10-24 17:39:46 +0700

    Update stocku.sql

--- Stocku/stocku.sql
+++ Stocku/stocku.sql
@@ -70,3 +70,69 @@ INSERT INTO Product (ID, namaCategory, namaProduct, Stok, harga, Deskripsi, Kome
 ('PRD020', 'Olahraga', 'Matras Yoga', 100, '300000', 'Matras anti-slip', NULL);
 
 SELECT * FROM Product;
+
+INSERT INTO Category (namaCategory) VALUES ('Rumah Tangga');
+
+ALTER TABLE Product 
+ADD Indikator VARCHAR(10) AFTER Stok,
+ADD Aksi VARCHAR(50) AFTER Indikator,
+ADD Prediksi VARCHAR(50) AFTER Aksi;
+
+ALTER TABLE Product DROP COLUMN Deskripsi;
+
+ALTER TABLE Product 
+MODIFY COLUMN Komentar VARCHAR(255) AFTER Harga;
+
+DESCRIBE Product;
+
+ALTER TABLE Product DROP COLUMN Komentar;
+ALTER TABLE Product DROP COLUMN Prediksi;
+
+DESCRIBE Product;
+
+ALTER TABLE Product ADD Cek VARCHAR(255) AFTER Aksi;
+ALTER TABLE Product ADD Prediksi DATE AFTER Cek;
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM Product;
+
+Select * From Product;
+
+ALTER TABLE Product MODIFY COLUMN ID VARCHAR(255) AFTER namaProduct;
+ALTER TABLE Product CHANGE COLUMN namaProduct Produk varchar(255);
+ALTER TABLE Product CHANGE COLUMN harga Harga varchar(255);
+ALTER TABLE Category DROP COLUMN Rumah_Tangga;
+
+SELECT * FROM Category;
+
+INSERT INTO Category (namaCategory) VALUES ('Rumah_Tangga');
+
+INSERT INTO Product (namaCategory, Produk, ID, Stok, Indikator, Aksi, Prediksi, Harga) VALUES
+('Elektronik', 'Smartphone A12', 'PRD001', 100, 'Hijau', 'Edit', NULL, 'Rp2.500.000,-'),
+('Elektronik', 'TV LED 32"', 'PRD002', 50, 'Merah', 'Edit', NULL, 'Rp3.200.000,-'),
+('Furniture', 'Sofa Minimalis', 'PRD003', 20, 'Merah', 'Edit', NULL, 'Rp1.800.000,-'),
+('Furniture', 'Lemari Pakaian', 'PRD004', 15, 'Merah', 'Edit', NULL, 'Rp2.200.000,-'),
+('Sepatu', 'Sneakers Hitam', 'PRD005', 120, 'Hijau', 'Edit', NULL, 'Rp450.000,-'),
+('Sepatu', 'Sandal Gunung', 'PRD006', 80, 'Hijau', 'Edit', NULL, 'Rp300.000,-'),
+('Tas', 'Ransel Laptop', 'PRD007', 70, 'Hijau', 'Edit', NULL, 'Rp550.000,-'),
+('Tas', 'Tas Selempang', 'PRD008', 90, 'Hijau', 'Edit', NULL, 'Rp350.000,-'),
+('Buku', 'Novel Fiksi', 'PRD009', 150, 'Hijau', 'Edit', NULL, 'Rp75.000,-'),
+('Buku', 'Komik Manga', 'PRD010', 200, 'Hijau', 'Edit', NULL, 'Rp45.000,-'),
+('Kosmetik', 'Lipstik Matte', 'PRD011', 300, 'Hijau', 'Edit', NULL, 'Rp120.000,-'),
+('Kosmetik', 'Foundation Cair', 'PRD012', 250, 'Hijau', 'Edit', NULL, 'Rp180.000,-'),
+('Otomotif', 'Oli Mesin 10W-40', 'PRD013', 500, 'Hijau', 'Edit', NULL, 'Rp75.000,-'),
+('Otomotif', 'Ban Mobil 185/70 R14', 'PRD014', 50, 'Merah', 'Edit', NULL, 'Rp950.000,-'),
+('Mainan', 'Lego Classic', 'PRD015', 80, 'Hijau', 'Edit', NULL, 'Rp850.000,-'),
+('Mainan', 'Boneka Teddy Bear', 'PRD016', 100, 'Hijau', 'Edit', NULL, 'Rp250.000,-'),
+('Alat Musik', 'Gitar Akustik', 'PRD017', 30, 'Merah', 'Edit', NULL, 'Rp1.500.000,-'),
+('Alat Musik', 'Keyboard Elektrik', 'PRD018', 10, 'Merah', 'Edit', NULL, 'Rp3.000.000,-'),
+('Olahraga', 'Sepeda Gunung', 'PRD019', 25, 'Merah', 'Edit', NULL, 'Rp2.500.000,-'),
+('Olahraga', 'Matras Yoga', 'PRD020', 100, 'Hijau', 'Edit', NULL, 'Rp300.000,-'),
+('Rumah_Tangga', 'Sunlight', 'PRD021', 150, 'Hijau', 'Edit', NULL, 'Rp25.000,-'),
+('Rumah_Tangga', 'Rinso', 'PRD022', 200, 'Hijau', 'Edit', NULL, 'Rp30.000,-'),
+('Rumah_Tangga', 'Tessa', 'PRD023', 180, 'Hijau', 'Edit', NULL, 'Rp20.000,-'),
+('Rumah_Tangga', 'Vixal', 'PRD024', 160, 'Hijau', 'Edit', NULL, 'Rp22.000,-'),
+('Rumah_Tangga', 'Baygon', 'PRD025', 140, 'Merah', 'Edit', NULL, 'Rp40.000,-'),
+('Rumah_Tangga', 'Downy', 'PRD026', 130, 'Hijau', 'Edit', NULL, 'Rp35.000,-');
+
+SELECT * FROM Product;
