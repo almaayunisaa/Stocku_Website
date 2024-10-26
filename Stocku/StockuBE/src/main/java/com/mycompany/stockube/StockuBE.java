531966b5186ef0f53992c4353ee2fec1064bfa18
almaayunisaa <almaayunisa8@gmail.coom>
2024-10-26 23:09:54 +0700

    Update Class
    menambah class di java backend

---
+++ Stocku/StockuBE/src/main/java/com/mycompany/stockube/StockuBE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stockube;
+
+/**
+ *
+ * @author Alma
+ */
+public class StockuBE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
