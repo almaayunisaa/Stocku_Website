6de1130cfb39f75478e9b33099b6d99360aecf90
Azizah Salma <147806636+almaayunisaa@users.noreply.github.com>
2025-04-22 20:26:09 +0700

    Update db.js

--- Stocku/StockuBE/IMPAL BE/config/db.js
+++ Stocku/StockuBE/IMPAL BE/config/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 // Membuat koneksi ke database
 const connection = 
 mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Alma2004!',
+    host:'',
+    user:'',
+    password:'',
     database:'stocku'
 })
 
